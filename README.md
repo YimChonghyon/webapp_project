0d452caed8bf1a18f9e3309119fd20a378b92946
jehyun Park <adam991217@naver.com>
2019-11-04 23:41:46 +0900

    Add basic file and register query Add php file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # webapp_project
+
+CSS 디자인 사용에 BOOTSTRAP 사용(MIT license)
+common 폴더에 이미지/자바스크랩트/css파일등 포함
+resoucre 폴더에 html/php 파일 포함
---
+++ resource/newquery.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>upload query</title>
+	<link rel="stylesheet" type="text/css" href="../common/styles/newquery.css" />
+	<link rel="stylesheet" href="../common/styles/bootstrap.min.css">
+</head>
+<body>
+	<header role="banner">
+		<div class="container">
+			<nav role="navigation">
+				<div id="logo" class="pull-left"><a href="/"><img src="../common/images/selab_logo_S.png" /></a></div>
+			</nav>
+		</div>
+	</header>
+
+	<main>
+		<div class="contents">
+			<h1>질문등록</h1>
+			<form action="">
+				<div class="btn-group-toggle" data-toggle="buttons" id="tag">
+					<?php
+					$tagnum = $_GET["tagnum"];
+					for ($i=0; $i < $tagnum; $i++) { ?>
+					<label class="btn btn-primary">
+						<input type="checkbox"> Checkbox
+					</label>
+					<?php } ?>
+				</div>
+				<div>
+					<textarea autofocus rows="20" cols="100" placeholder="Type your query."></textarea>
+				</div>
+				<div class="overlap-height">
+					<input type="checkbox" name="anonymous" /> 익명
+				</div>
+				<div class="contents">
+					<input type="submit" value="등록" class="btn btn-info BUT" />
+					<input type="button" value="취소" class="btn btn-info BUT" />
+				</div>
+			</form>
+		</div>
+	</main>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="../common/js/bootstrap.min.js"></script>
+</body>
+</html>
