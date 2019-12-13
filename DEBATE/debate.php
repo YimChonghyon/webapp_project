d103afabb59770996546aec9543a978f9eb0bca6
임정현 <yimchonghyon@imjeonghyeon-ui-MacBookPro.local>
2019-12-13 19:59:38 +0900

    debate

--- DEBATE/debate.php
+++ DEBATE/debate.php
@@ -10,17 +10,17 @@
   include "../resource/nav.php";
   ?>
 	<main>
-		<div class="in_nav pull-left">
+		<!-- <div class="in_nav pull-left">
 			<a href="" class="pull-left"><h1>Web Application Development</h1></a>
-				<!-- <img class= "out_button pull-right" src="../resource/images/out.png"> -->
-		</div>
+				<img class= "out_button pull-right" src="../resource/images/out.png">
+		</div> -->
 
 		<div class="list scroll">
 			<form method="post" action="">
 				<div class="search">
 					<div>
-					<input type="text" name="" id="searchbox" name="search" >
-					<button type="submit">
+					<input type="text" name="" id="searchbox" size="30"  >
+					<button type="submit" class="button">
 					<image class="pull-right search_icon" src="../resource/images/search.png">
 				</button>
 					</div>
@@ -189,6 +189,97 @@
 								23:03
 							</div>
 						</div>
+
+						<div class="icon_container ">
+							<div class="question_container">
+									<img class="question_icon" src="../resource/images/question.png">
+									<div class="question_icon_num">
+										3
+									</div>
+							</div>
+							<div class="comment_container ">
+									<img class="comment_icon" src="../resource/images/comment.png">
+									<div class="comment_icon_num"> 
+										1
+									</div>
+								</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			<div class="debate_content_container">
+				<div>
+					<div class="title_tag">
+						<div class="tag_number">
+							#1
+						</div>
+						<div class="tag_catagory">
+							#공지
+						</div>
+					</div>
+				
+				
+					<div class="titlebox">
+						<div class="title_text">
+							안녕하세요. 제목입니다.
+						</div>
+					</div>
+				
+					<div class="bottom_infor">
+						<div class="write_infor">
+							<div class="name">
+								임정현
+							</div>
+							<div class="time">
+								23:03
+							</div>
+						</div>
+						<div class="icon_container pull-right">
+							<div class="question_container pull-right">
+									<img class="question_icon" src="../resource/images/question.png">
+									<div class="question_icon_num">
+										3
+									</div>
+							</div>
+							<div class="comment_container pull-right">
+									<img class="comment_icon" src="../resource/images/comment.png">
+									<div class="comment_icon_num"> 
+										1
+									</div>
+								</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			<div class="debate_content_container">
+				<div>
+					<div class="title_tag">
+						<div class="tag_number">
+							#1
+						</div>
+						<div class="tag_catagory">
+							#공지
+						</div>
+					</div>
+				
+				
+					<div class="titlebox">
+						<div class="title_text">
+							안녕하세요. 제목입니다.
+						</div>
+					</div>
+				
+					<div class="bottom_infor">
+						<div class="write_infor">
+							<div class="name">
+								임정현
+							</div>
+							<div class="time">
+								23:03
+							</div>
+						</div>
 						<div class="icon_container pull-right">
 							<div class="question_container pull-right">
 									<img class="question_icon" src="../resource/images/question.png">
@@ -217,6 +308,7 @@
 		</form>
 		</div>
 		<div class="debatebox scroll">
+			<div class="debate_content_container_maincontainer">
 			<div class="debate_content_container_main">
 				<div class="title_tag"> 
 					<div class="tag_number">
@@ -255,10 +347,77 @@
 					</div>
 				</form>	
 			</div>
+</div>
 		
 				
 			
 			<hr />
+			<div class="debate_reply">
+			<div class="comment_container">
+				<div class="commentbox">
+					<div class="name">
+						임정현
+					</div>
+					
+					<div class="comment_contents_box"> 
+						<div class="comment">
+							<div clss="comment_text">
+								최고에요!
+							</div>
+						</div>
+						<div class="comment_additional">
+							<div class= "comment_time">
+								오후 11:30
+							</div>
+							<div class= "heart">
+								<form method="POST">
+								<button type="submit" class ="button">
+									<img class = "heart_button" src="../resource/images/hearts.png">
+								</button>
+							</form>
+							<div class="heart_num">
+								3
+							</div>
+							</div>
+
+						</div>
+					</div>
+					
+				</div>
+			</div>
+			<div class="debate_reply">
+			<div class="comment_container">
+				<div class="commentbox">
+					<div class="name">
+						임정현
+					</div>
+					
+					<div class="comment_contents_box"> 
+						<div class="comment">
+							<div clss="comment_text">
+								최고에요!
+							</div>
+						</div>
+						<div class="comment_additional">
+							<div class= "comment_time">
+								오후 11:30
+							</div>
+							<div class= "heart">
+								<form method="POST">
+								<button type="submit" class ="button">
+									<img class = "heart_button" src="../resource/images/hearts.png">
+								</button>
+							</form>
+							<div class="heart_num">
+								3
+							</div>
+							</div>
+
+						</div>
+					</div>
+					
+				</div>
+			</div>
 			<div class="debate_reply">
 			<div class="comment_container">
 				<div class="commentbox">
