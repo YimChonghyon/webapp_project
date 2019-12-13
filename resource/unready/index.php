c42ecc1eb65b3caa683c4d92afbddaba54d1b09f
sjlee523 <55562875+sjlee523@users.noreply.github.com>
2019-12-13 16:15:28 +0900

    .

--- resource/unready/index.php
+++ resource/unready/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
-<?php session_start(); ?>
+
 <html>
 <head>
     <meta charset="utf-8" />
@@ -15,22 +15,18 @@
     <style>
     *{margin:0;padding:0;}
     ul,li{list-style:none;}
-    .slide{height:500px;overflow:hidden;}
-    .slide ul{width:calc(100% * 4);display:flex;animation:slide 8s infinite;} /* slide 8초 동안 진행, 반복*/
-    .slide li{width:calc(100% / 4);height:500px;}
+    .slide{height:500px; overflow:hidden;}
+    .slide ul{width:calc(100% * 4); display:flex; animation:slide 6s infinite;} /* slide 8초 동안 진행, 반복*/
+    .slide li{width:calc(100% / 4); height:500px;}
     .slide li:nth-child(1){background:#ffa;}
     .slide li:nth-child(2){background:#faa;}
-    .slide li:nth-child(3){background:#afa;}
-    .slide li:nth-child(4){background:#aaf;}
+
     @keyframes slide {
-      0% {margin-left:0;} /* 0 ~ 10  : 정지 */
-      10% {margin-left:0;} /* 10 ~ 25 : 변이 */
-      25% {margin-left:-100%;} /* 25 ~ 35 : 정지 */
-      35% {margin-left:-100%;} /* 35 ~ 50 : 변이 */
-      50% {margin-left:-200%;}
-      60% {margin-left:-200%;}
-      75% {margin-left:-300%;}
-      85% {margin-left:-300%;}
+      0% {margin-left:0;}
+      20% {margin-left:0;}
+      40% {margin-left:-100%;}
+      60% {margin-left:-100%;}
+      80% {margin-left:-200%;}
       100% {margin-left:0;}
     }
 
@@ -54,6 +50,7 @@
        outline:none;
    }
   
+
    .menuBtn i {
        color:#888888;
    }
@@ -83,6 +80,8 @@
 }
 
   </style>
+
+
     <script src="http://code.jquery.com/jquery-1.12.4.js"></script>
     <script type="text/javascript">
         jQuery(document).ready(function() {
@@ -92,12 +91,11 @@
     });
     });
     </script>
+    <script src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"></script>
 </head>
 
 <body>    
-<?php try{$conn=new PDO("mysql:host=localhost;dbname=selab","root","root");$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);}catch(PDOException $e){
-    //echo "Connection failed: " . $e->getMessage();
-}?>
+
         <button type="button" class="menuBtn">
         <i><img src="icon-menu.svg" style="width:20px;height:20px;"/></i><span>MENU</span>
         </button>
@@ -114,14 +112,7 @@
                     <li class="pull-left"><a class= "active" href="resource/main_course.php" >COURSES</a></li>
                     <li class="pull-left"><a class= "active" href="resource/gallery" >GALLERY</a></li>
                 </ul>
-                <?php
-                if(!isset($_SESSION['id']))
-                    echo '<div role= "login" class= "pull-right"><a href= "resource/login.php">LOGIN</a></div>';
-                else{
-                    echo '<div role= "logout" class= "pull-right"><a href= "resource/logout.php">LOGOUT</a></div>';
-                    echo '<div role= "setting" class= "pull-right"><a href= "resource/setting.php">SETTING</a></div>';
-                }
-                ?>
+                
 
             </nav>
         </header>
@@ -129,10 +120,8 @@
                 <!-- INDEX MAIN IMAGE -->
                 <div class="slide">
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li><a href="#"><img src="main_1.jpg" style="max-width: 100%; height: auto;"/></li>
+                    <li><a href="#"><img src="main_2.jpg" style="max-width: 100%; height: auto;"/></li>
                 </ul>
                 </div>
 
@@ -140,7 +129,9 @@
             <div id= "index_layout_1">
                 <!-- NOTICE -->
             <article id="index_notice">
-                <h3><a href="resource/notice.html">NOTICE</a></h3> <!-- 기본적인 notice 링크 -->
+            <br>
+                <h3><a href="resource/notice.html">NOTICE&nbsp;&nbsp;<i class="fas fa-bell"></i></a></h3> <!-- 기본적인 notice 링크 -->
+                <br>
                 <p>Content Start</p>
                 <p>Content</p>
                 <p>Content</p>
@@ -150,8 +141,9 @@
 
                 <!-- COURSES -->
             <article id="index_courses">
-                <h3><a href="/courses">COURSES</a></h3> <!-- 이번 학기에 열린 강의 목록 / 링크 -->
-
+            <br>
+                <h3><a href="/courses">COURSES&nbsp;&nbsp;<i class="fab fa-discourse"></i></a></h3> <!-- 이번 학기에 열린 강의 목록 / 링크 -->
+                <br>
                 <p>Content Start</p>
                 <p>Content</p>
                 <p>Content</p>
@@ -163,7 +155,9 @@
             <div id="index_layout_2">
                 <!-- ABOUT US -->
             <article id="index_aboutus">
-                <h3><a href="/research">ABOUT US</a></h3>
+            <br>
+                <h3><a href="/research">ABOUT US&nbsp;&nbsp;<i class="fas fa-address-card"></i></a></h3>
+                <br>
                 <p>welcome to Software Engineering Laboratory within the CSE Dept @ HYU(ERICA) led by asst. prof. Scott LEE</p>
                 <p>Content Start</p> <!-- 사진 내용 3개 -->
                 <p>Content</p>
@@ -173,7 +167,9 @@
             </article>
 
             <article id="index_map">
-                <h3><a href="/research">CONTACT</a></h3>
+            
+                <h3><a href="/research">CONTACT&nbsp;&nbsp;<i class="fas fa-map-pin"></i></i></a></h3>
+                <br>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3173.9242122905407!2d126.83208981526195!3d37.296929979849345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b6ef2f5a7cb73%3A0x26b59cb3d9af4a46!2z7ZWc7JaR64yA7ZWZ6rWQIEVSSUNB7Lqg7Y287Iqk!5e0!3m2!1sko!2skr!4v1575792959595!5m2!1sko!2skr"
                     width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="">
                 </iframe>
@@ -186,5 +182,5 @@
         </div>
     </footer>
 </body>
-<?php $conn = null; // disconnect db ?>
+
 </html>
