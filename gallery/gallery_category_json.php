c25a770ae55276ef800b66165e5a0869233207d6
jehyun Park <adam991217@naver.com>
2019-12-14 22:34:52 +0900

    add gallery category ajax

---
+++ gallery/gallery_category_json.php
@@ -0,0 +1,18 @@
+<?php
+include "../resource/DB_connect.php";
+$conn = connect();
+$stmt = $conn->prepare("select type from gallery_type");
+$stmt -> execute();
+$result = $stmt -> fetchAll();
+
+print "{\n  \"categorys\": [\n";
+$type = 1;
+foreach ($result as $key => $value) {
+	if($type == 0)
+		print ",\n";
+	else
+		$type = 0;
+	print '"' . $value['type'] . '"';
+}
+print "\n  ]\n}\n";
+?>
