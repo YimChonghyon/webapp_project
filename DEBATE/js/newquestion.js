288bf762f6f745ae07d6887f1ab3591ba2bb35b1
jehyun Park <adam991217@naver.com>
2019-12-20 01:29:53 +0900

    d

--- DEBATE/js/newquestion.js
+++ DEBATE/js/newquestion.js
@@ -5,6 +5,10 @@ window.onload = function() {
 		onFailure : ajaxFailed,
 		onException : ajaxFailed
 	});
+	$('debate_anonymous').checked = true;
+	$('debate_name').disabled = true;
+	$('debate_name').value = 'anonymous';
+	$('debate_anonymous').observe('click', checking_anonymous_button);
 }
 
 function tag_JSON(ajax){
@@ -37,4 +41,39 @@ function ajaxFailed(ajax, exception) {
 
 function Close() {
 	self.close();
+}
+
+var idmemory = "";
+function checking_anonymous_button() {
+	//익명 체크 버튼 체크 상태에 따라 ID 입력칸 비활성화
+	if(this.checked == true){
+		$('debate_name').disabled = true;
+		idmemory = $('debate_name').value;
+		$('debate_name').value = 'anonymous';
+	}
+	else{
+		$('debate_name').disabled = false;
+		$('debate_name').value = idmemory;
+	}
+}
+
+function check_debate_blank() {
+	//질문 작성시 포맷을 제대로 작성했는지 검사
+
+	if($('debate_anonymous').checked == false && !$('debate_name').value){
+		alert('아이디를 입력해 주세요.');
+		return false;
+	}
+
+	if(!$('debate_password').value){
+		alert('비밀번호를 입력해 주세요.');
+		return false;
+	}
+
+	if(!$('debate_content').value){
+		alert('댓글을 입력해 주세요.');
+		return false;
+	}
+
+	return true;
 }
